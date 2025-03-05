80096719b36ea6d69256ab7b065172127d9f5416
Marie <lenvik.marie@gmail.com>
2025-03-05 21:28:12 +0100

    Update vite.config.js
    added alias @

--- vite.config.js
+++ vite.config.js
@@ -2,16 +2,19 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import path from 'path';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
   base: '/fed24d-grafiska-verktyg-panterkameleonterna/',
-
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
   css: {
     preprocessorOptions: {
       scss: {
-        additionalData: `@import "./scss/_variables.scss";`,
+        additionalData: `@import "@/scss/_variables.scss";`,
       },
     },
   },
-})
+});
