6b9e3c2215bdaea41be7d64746fae36e4264e9c7
David Brunni <davidbrunni@hotmail.com>
2025-03-07 14:54:55 +0100

    Quickfix

--- src/views/BookingView.vue
+++ src/views/BookingView.vue
@@ -1,8 +1,8 @@
 <script setup>
-
+import NavBar from "../components/NavBar.vue";
 import BookingCards from "../components/BookingCards.vue";
 import MainFooter from "../components/MainFooter.vue";
-import MobileHeader from "../components/MobileHeader.vue";
+import NewMobileHeader from "../components/NewMobileHeader.vue";
 </script>
 
 <template>
