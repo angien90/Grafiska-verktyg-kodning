17909ea49e1f69565c958f35bf438535e37baedb
tgvie <166875380+tgvie@users.noreply.github.com>
2025-03-07 04:39:59 +0100

    add footer to BookingView

--- src/views/BookingView.vue
+++ src/views/BookingView.vue
@@ -1,10 +1,12 @@
 <script setup>
 import BookingCards from '../components/BookingCards.vue';
+import MainFooter from '../components/MainFooter.vue';
 </script>
 
 <template>
   <div>
     <BookingCards />
+    <MainFooter />
   </div>
 </template>
 
