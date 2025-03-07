1b6137e817ad63b8ed61197c0c910f8fc4915ae3
M-Lenvik <lenvik.marie@gmail.com>
2025-03-07 14:42:38 +0100

    feat: (index.html) added SEO metatags

---
+++ src/components/NavBar.vue
@@ -0,0 +1,75 @@
+<template>
+  <nav>
+    <ul>
+      <li><router-link to="/" exact-active-class="active">Hem</router-link></li>
+      <li>Om oss</li>
+      <li><router-link to="/booking" exact-active-class="active">Boka rum</router-link></li>
+      <li>Kontakta oss</li>
+    </ul>
+  </nav>
+</template>
+
+<style lang="scss" scoped>
+@media (max-width: 799px) {
+  nav {
+    display: none;
+  }
+}
+
+@media (min-width: 800px) {
+  nav {
+    background-color: $skogsgron;
+    width: 100%;
+    height: 60px;
+    display: flex;
+
+    ul {
+      width: 100%;
+      display: flex;
+      flex-direction: row;
+      list-style-type: none;
+      justify-content: space-between;
+      align-items: center;
+      padding: 0 128px 0 128px; //enligt layout i komponenter
+
+      li {
+        font-family: $heading-font;
+        font-size: 25px;
+        text-transform: uppercase;
+        color: white;
+
+        a {
+          text-decoration: none;
+          text-align: center;
+
+          &:visited {
+            color: white;
+          }
+
+          &.active {
+            border-bottom: 3px solid;
+          }
+
+          &:hover {
+            color: $morkbrun;
+          }
+        }
+      }
+    }
+  }
+}
+
+@media (min-width: 1000px) {
+  nav {
+    height: 130px;
+
+    ul {
+      padding: 0 92px 0 92px; //enligt layout i komponenter
+
+      li {
+        font-size: 40px; //enligt layout i komponenter
+      }
+    }
+  }
+}
+</style>
