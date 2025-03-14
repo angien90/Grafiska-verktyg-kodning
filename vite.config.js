674a874737273cb149714b9556ff39e81fc4335c
Angelica Nylander <angelicanylander@hotmail.com>
2025-03-14 15:09:29 +0100

    Update vite.config.js
    Change back to original vite-config.js code

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,20 @@
 import { defineConfig } from "vite";
- import vue from "@vitejs/plugin-vue";
- import path from "path";
- 
- export default defineConfig({
-   plugins: [vue()],
-   base: "/fed24d-grafiska-verktyg-panterkameleonterna/",
-   resolve: {
-     alias: {
-       "@": path.resolve(__dirname, "src"),
-     },
-   },
-   css: {
-     preprocessorOptions: {
-       scss: {
-         additionalData: `@import "@/scss/_variables.scss";`,
-       },
-     },
-   },
-}));
+import vue from "@vitejs/plugin-vue";
+import path from "path";
+
+export default defineConfig({
+  plugins: [vue()],
+  base: "/fed24d-grafiska-verktyg-panterkameleonterna/",
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+    },
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@import "@/scss/_variables.scss";`,
+      },
+    },
+  },
+});
