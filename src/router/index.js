2c4ef71ee65c567687fbc70082050288a0214cb8
Angelica Nylander <angelicanylander@hotmail.com>
2025-03-14 15:17:57 +0100

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -16,7 +16,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHistory("/fed24d-grafiska-verktyg-panterkameleonterna/")
+  history: createWebHistory(import.meta.env.BASE_URL),
   routes
 });
 
