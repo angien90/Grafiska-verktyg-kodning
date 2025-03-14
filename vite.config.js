a038761147b26ab95c79978b66783681a3ef3d74
Angelica Nylander <angelicanylander@hotmail.com>
2025-03-14 14:37:10 +0100

    Update vite.config.js
    Update vite.config.js after last change dosnt work

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,9 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import path from "path";
 
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [vue()],
-  base: "/fed24d-grafiska-verktyg-panterkameleonterna/",
+  base: mode === "production" ? "/fed24d-grafiska-verktyg-panterkameleonterna/" : "/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
@@ -17,9 +17,4 @@ export default defineConfig({
       },
     },
   },
-  module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/fed24d-grafiska-verktyg-panterkameleonterna/'
-    : '/'
-}
-});
+}));
